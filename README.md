afcfa64114d4fa5bbe0211e1986b72803cb5530a
ItsmeLee <arsmagica90@gmail.com>
2021-01-05 13:33:03 +0900

    Add key sequence detect function
    Add key sequence detect function in index.js file.
    When user pressed secret key, display rainbow pictures in main page.

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ Make a key Sequence Detection using Vanilla JavaScript.
 # To Do
 
 - [x] Key up event.
-- [ ] Key Detect function.
+- [x] Key Detect function.
 
 # Work Flow
 
 - [x] Key up event.
-- [ ] Key Detect function.
+- [x] Key Detect function.
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 const pressed = [];
+const secretKey = 'webos';      // secretKey.length = 6
 
 function handleKeyUp(event){
     console.log(event.key);
     pressed.push(event.key);
+    pressed.splice(-secretKey.length-1, pressed.length - secretKey.length);
+    if(pressed.join('').includes(secretKey)){
+        console.log(`You Did it!`);
+        cornify_add();
+    }
     console.log(pressed);
 }
 
